9eb75e2fadae5b949217463bbe2c1587cbc3d225
Kiss Gabor <nevi@agmen.hu>
2019-12-30 12:19:55 +0100

    openclass action added to symfony kind

--- rplugin/python3/symfony/denite/kind/symfony.py
+++ rplugin/python3/symfony/denite/kind/symfony.py
@@ -9,8 +9,11 @@ class Kind(Base):
         self.default_action = 'open'
 
     def action_open(self, context: UserContext) -> None:
+        self._openTargets(context, 'inplace')
+
+    def _openTargets(self, context, mode):
         for target in context['targets']:
-            self.vim.call(self._getOpenFunction(target), self._getWord(target), 'inplace')
+            self.vim.call(self._getOpenFunction(target), target['word'], mode)
 
     def _getOpenFunction(self, target):
         if 'action__type' not in target:
@@ -25,20 +28,36 @@ class Kind(Base):
 
         raise Exception('Unknown action type: ' + target['action__type'])
 
-    def _getWord(self, target):
-        if target['action__type'] is 'entity':
-            return target['word'].replace('\\\\', "\\")
-
-        return target['word']
-
     def action_split(self, context: UserContext) -> None:
-        for target in context['targets']:
-            self.vim.call(self._getOpenFunction(target), self._getWord(target), 'split')
+        self._openTargets(context, 'split')
 
     def action_vsplit(self, context: UserContext) -> None:
-        for target in context['targets']:
-            self.vim.call(self._getOpenFunction(target), self._getWord(target), 'vsplit')
+        self._openTargets(context, 'vsplit')
 
     def action_tabopen(self, context: UserContext) -> None:
+        self._openTargets(context, 'tab')
+
+    def action_openclass(self, context: UserContext) -> None:
+        self._openTargetsClass(context, 'inplace')
+
+    def _openTargetsClass(self, context, mode):
         for target in context['targets']:
-            self.vim.call(self._getOpenFunction(target), self._getWord(target), 'tab')
+            cls = self._getClassFromTarget(target)
+            if not cls:
+                continue
+            self.vim.call('symfony#goto#class', cls, mode)
+
+    def _getClassFromTarget(self, target):
+        if 'action__class' in target:
+            return target['action__class']
+        if 'action__type ' in target and target['action__type'] is 'entity':
+            return target['word']
+
+    def action_splitclass(self, context: UserContext) -> None:
+        self._openTargetsClass(context, 'split')
+
+    def action_vsplitclass(self, context: UserContext) -> None:
+        self._openTargetsClass(context, 'vsplit')
+
+    def action_tabopenclass(self, context: UserContext) -> None:
+        self._openTargetsClass(context, 'tab')
